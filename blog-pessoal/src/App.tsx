10d16502dddbba645aabf8bcfb7b6a57b2855af3
Edu-sousa <desousa.eduardo09@gmail.com>
2022-01-13 15:22:37 -0300

    primeiros conceitos de react

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+
+let nome ='Eduardo'
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
